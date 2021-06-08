b76e96e78d03d9b0b6ba2189eebac55e883f3924
lealdi <diegoleal1913@gmail.com>
2021-06-08 13:43:25 -0300

    funcionalidades do slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+const sabores = [
+    {
+    sabor: 'Marguerita',
+    descricao: [`Molho de tomate natural temperado com azeite, sal e orégano,
+    Queijo mussarela fatiado.
+    Orégano e manjericão fresco.`]
+    },
+]
+
+console.log(sabores[0])
+
+function arrowLeft(){
+    let arrowleft = document.getElementById('arrow-left')
+    let paragraphSec2 = document.getElementById('hide-psec2')
+    console.log(paragraphSec2)
+
+    paragraphSec2.innerHTML = `${sabores[0].descricao}`
+    
+}
+
+
+
+// Manipular o slider
 new Glide(".images", {
     type: 'carousel',
     perView: 1,
